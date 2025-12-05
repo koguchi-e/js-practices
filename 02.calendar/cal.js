b64bdbeaa20fc13fa442a9d81bd0687fcde04833
koguchi-e <koguchi.e915@gmail.com>
2025-12-05 16:52:32 +0900

    文字幅の修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -14,12 +14,19 @@ const { values, positionals } = parseArgs({
   options
 });
 
-const year = Number(values.y);
-const month = Number(values.m) ;
+let year;
+if (values.y !== undefined){
+  year = Number(values.y);
+}else{
+  year = 2025;
+}
 
+const month = Number(values.m) ;
 const date = new Date(year, month)
 
-console.log(`${month}月 ${year}`)
+const title = `${month}月 ${year}`
+
+console.log(title.padStart(14, " "));
 console.log('日 月 火 水 木 金 土');
 
 const first_wday = new Date(year, month -1 , 1).getDay();
@@ -35,75 +42,7 @@ for (let num = 1; num <= first_wday; num++ ){
 }
 
 for (let i = 0; i < all_days.length; i += 7) {
-  const calender_days = all_days.slice(i, i + 7);
-  console.log(calender_days.join(" "));
+  const calender_days = (all_days.slice(i, i + 7));
+  const result = calender_days.map(n => String(n).padStart(2, " "));
+  console.log(result.join(" "));
 }
-
-// console.log(all_days.join(" "));
-
-// for (let i = 0; i < all_days.length; i += 7){
-//   console/log(all_days.slice(i, i + 7));
-// }
-
-// const calender_days = (all_days.join(" "));
-
-// let limit = 7;
-// let newText = "";
-// for (let i = 0; i < calender_days.length; i += limit) {
-//   newText += calender_days.substring(i, i + limit) + "\n";
-// }
-// console.log(newText);
-
-
-// const eachSlice = (arr, n = 2, result = []) => {
-//   if (arr.length === 0) {
-//     return result;
-//   }
-
-//   result.push(arr.splice(0, n))
-//   return eachSlice(arr, n, result)
-// }
-
-// console.log(eachSlice(calender_days, 7));
-
-// console.log(all_days);
-
-// console.log(first_wday);
-
-// const chunkIntoN = (arr, n) => {
-//   const size = arr.length / n;
-//   return Array.from({ length: n }, (v, i) =>
-//     arr.slice(i * size, i * size + size)
-//   );
-// };
-
-// console.log(chunkIntoN(all_days, 7));
-
-// const b = all_days.length;
-// const cnt = 7,
-// newArr = [];
-// for(var i = 0; i < Math.ceil(b / cnt); i++) {
-//   var arrEl = []; // newArr に追加していく配列
-//   for(var j = 0; j < cnt; j++) {
-//     var p = all_days.shift(); // 元配列の先頭を取得して元配列から削除
-//     if(!p) { break; }        // この場合は'ZZZ'の次に来たらループを抜ける
-//     arrEl.push(p);           // arrElに取得した要素を追加
-//   }
-//   newArr.push(arrEl); // 作ったarrElをnewArrに追加
-//   console.log(newArr);
-// }
-
-// console.log(newArr);
-
-// const sliceByNumber = (array, number) => {
-//   const length = Math.ceil(array.length / number)
-//   return new Array(length).fill().map((_, i) =>
-//     array.slice(i * number, (i + 1) * number)
-//   )
-// }
-// console.log(sliceByNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3))
-
-
-
-
-

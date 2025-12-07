f050680c05252eb6a7bd5f52223f19f6f3666492
koguchi.e <koguchi.e915@gmail.com>
2025-12-07 15:37:38 +0900

    月も指定なしで今月が取得できるよう修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,53 +1,57 @@
 #!/usr/bin/env node
 
-import { log } from 'node:console';
-import { type } from 'node:os';
-import { parseArgs } from 'node:util';
+import { parseArgs } from "node:util";
 
 const options = {
-  y: { type: 'string' },
-  m: { type: 'string' },
+  y: { type: "string" },
+  m: { type: "string" },
 };
 
-const { values, positionals } = parseArgs({
+const today = new Date();
+
+const { values } = parseArgs({
   args: process.argv.slice(2),
-  options
+  options,
 });
 
 let year;
-if (values.y !== undefined){
+if (values.y !== undefined) {
   year = Number(values.y);
-}else{
-  year = 2025;
+} else {
+  year = today.getFullYear();
 }
 
-const month = Number(values.m) ;
-const date = new Date(year, month)
+let month;
+if (values.m !== undefined) {
+  month = Number(values.m);
+} else {
+  month = today.getMonth() + 1;
+}
 
-const title = `${month}月 ${year}`
+const title = `${month}月 ${year}`;
 
 console.log(title.padStart(14, " "));
-console.log('日 月 火 水 木 金 土');
+console.log("日 月 火 水 木 金 土");
 
-const first_wday = new Date(year, month -1 , 1).getDay();
+const first_wday = new Date(year, month - 1, 1).getDay();
 const last_day = new Date(year, month, 0).getDate();
 
-const all_days = []
-for (let day = 1; day <= last_day; day++ ){
-  all_days.push(day)
+const all_days = [];
+for (let day = 1; day <= last_day; day++) {
+  all_days.push(day);
 }
 
-for (let num = 1; num <= first_wday; num++ ){
+for (let num = 1; num <= first_wday; num++) {
   all_days.unshift(null);
 }
 
 for (let i = 0; i < all_days.length; i += 7) {
-  const calender_days = (all_days.slice(i, i + 7));
+  const calender_days = all_days.slice(i, i + 7);
 
-  const result = calender_days.map(n => {
+  const result = calender_days.map((n) => {
     if (n === null) {
       return String(n).replace("null", " ").padStart(2, " ");
-    }else{
+    } else {
       return String(n).padStart(2, " ");
     }
   });

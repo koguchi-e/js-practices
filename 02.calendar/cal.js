dd8cbb81692eaa102629640bbb4215dcce02ab4d
koguchi-e <koguchi.e915@gmail.com>
2025-12-11 14:03:32 +0900

    変数をキャメルケースに修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -25,22 +25,22 @@ const title = `${monthStr}月 ${year}`;
 console.log(title.padStart(monthStr.length === 1 ? 13 : 14, " "));
 console.log("日 月 火 水 木 金 土");
 
-const first_wday = new Date(year, month - 1, 1).getDay();
-const last_day = new Date(year, month, 0).getDate();
+const firstDayOfWeek = new Date(year, month - 1, 1).getDay();
+const lastDay = new Date(year, month, 0).getDate();
 
-const all_days = [];
-for (let day = 1; day <= last_day; day++) {
-  all_days.push(day);
+const allDays = [];
+for (let day = 1; day <= lastDay; day++) {
+  allDays.push(day);
 }
 
-for (let num = 1; num <= first_wday; num++) {
-  all_days.unshift(null);
+for (let num = 1; num <= firstDayOfWeek; num++) {
+  allDays.unshift(null);
 }
 
-for (let i = 0; i < all_days.length; i += 7) {
-  const calender_days = all_days.slice(i, i + 7);
+for (let i = 0; i < allDays.length; i += 7) {
+  const calendarDays = allDays.slice(i, i + 7);
 
-  const result = calender_days.map((n) => {
+  const result = calendarDays.map((n) => {
     if (n === null) {
       return String(n).replace("null", " ").padStart(2, " ");
     } else {

02d9c544267774fa95ef8b8b45144cc8f2cb98cd
koguchi-e <koguchi.e915@gmail.com>
2025-12-12 16:23:22 +0900

    コールバック関数でエラーなしの処理を作成

--- 03.asynchronous/asynchronous.js
+++ 03.asynchronous/asynchronous.js
@@ -1,7 +1,30 @@
 #!/usr/bin/env node
 
-console.log("test");
-// const sqlite3 = require("sqlite3").verbose();
-// const db = new sqlite3.Database(":memory:");
+// import { useCallback } from "react";
+import sqlite3 from "sqlite3";
+const db = new sqlite3.Database(":memory:");
 
-// db.serialize;
+// DBをコールバックで作成
+function makeTableCallback() {
+  setTimeout(() => {
+    db.run("CREATE TABLE books (id INT, title TEXT)");
+    const stmt = db.prepare("INSERT INTO books VALUES (id, ?)");
+    stmt.run([1, "走れメロス"]);
+    stmt.run([2, "こころ"]);
+    stmt.run([3, "山月記"]);
+    stmt.finalize();
+    // db.each("SELECT rowid AS id, info FROM books", (err, row) => {
+    //   console.log(row.id + ": " + row.title);
+    //   // useCallback(null, stmt);
+    // });
+  });
+}
+db.close();
+
+makeTableCallback("SELECT rowid AS id, info FROM books", (err, row) => {
+  if (err) {
+    console.log("エラー:", err);
+    return;
+  }
+  console.log(row.id + ": " + row.title);
+});

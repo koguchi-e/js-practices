eab326c993b1395b3b9444f0b92d2a14877d82e4
koguchi-e <koguchi.e915@gmail.com>
2025-12-12 12:31:20 +0900

    eslint.config.jsも元の状態に戻す

--- 02.calendar/eslint.config.js
+++ 02.calendar/eslint.config.js
@@ -3,12 +3,7 @@ import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-  {
-    languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
-      sourceType: "module",
-    },
-  },
+  { languageOptions: { globals: globals.nodeBuiltin } },
   pluginJs.configs.recommended,
   eslintConfigPrettier,
 ];

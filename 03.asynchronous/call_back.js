9b8015caded85cd007df73bab9af2bbbca5844f7
koguchi-e <koguchi.e915@gmail.com>
2025-12-18 13:55:02 +0900

    エラーありのコードを作成

--- 03.asynchronous/call_back.js
+++ 03.asynchronous/call_back.js
@@ -3,36 +3,21 @@
 import sqlite3 from "sqlite3";
 const db = new sqlite3.Database(":memory:");
 
-function makeTableCallback(callback) {
+// エラーなし
+function successFlow(callback) {
   setTimeout(() => {
     db.run(
       "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL)",
-      (err) => {
-        if (err) {
-          callback(err);
-          return;
-        }
+      () => {
         const stmt = db.prepare("INSERT INTO books (title) VALUES (?)");
-        stmt.run(["走れメロス"], function (err) {
-          if (err) {
-            callback(err);
-            return;
-          }
-          stmt.run(["こころ"], function (err) {
-            if (err) {
-              callback(err);
-              return;
-            }
-            stmt.run(["山月記"], function (err) {
+        stmt.run(["走れメロス"], function () {
+          stmt.run(["こころ"], function () {
+            stmt.run(["山月記"], function () {
               stmt.finalize();
-              if (err) {
-                callback(err);
-                return;
-              }
               db.all("SELECT id, title FROM books ORDER BY id", (err, rows) => {
                 db.run("DROP TABLE books", (err) => {
                   if (err) {
-                    callback(err);
+                    callback(new Error("失敗しました"));
                     return;
                   }
                   callback(null, rows);
@@ -46,9 +31,42 @@ function makeTableCallback(callback) {
   }, 1000);
 }
 
-makeTableCallback((err, rows) => {
+successFlow((err, rows) => {
   if (err) {
-    console.error("エラー:", err);
+    console.error("エラー:", err.message);
+    return;
+  }
+  rows.forEach((row) => {
+    console.log(row.id + ": " + row.title);
+  });
+});
+
+// エラーあり
+function failureFlow(callback) {
+  setTimeout(() => {
+    db.run(
+      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL)",
+      () => {
+        const stmt = db.prepare("INSERT INTO books (title) VALUES (?)");
+        stmt.run([null], function () {
+          db.all("SELECT id, title FROM books ORDER BY id", (err, rows) => {
+            db.run("DROP TABLE books", () => {
+              if (err) {
+                callback(new Error("失敗しました"));
+                return;
+              }
+              callback(null, rows);
+            });
+          });
+        });
+      },
+    );
+  }, 1000);
+}
+
+failureFlow((err, rows) => {
+  if (err) {
+    console.error("エラー:", err.message);
     return;
   }
   rows.forEach((row) => {

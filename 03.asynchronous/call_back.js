3e85bdab4c2897fcb07e6fde43a9850ce34f1b4e
koguchi-e <koguchi.e915@gmail.com>
2025-12-18 11:37:23 +0900

    IDを自動採番するように変更

--- 03.asynchronous/call_back.js
+++ 03.asynchronous/call_back.js
@@ -5,39 +5,42 @@ const db = new sqlite3.Database(":memory:");
 
 function makeTableCallback(callback) {
   setTimeout(() => {
-    db.run("CREATE TABLE books (id INT, title TEXT)", (err) => {
-      if (err) {
-        callback(err);
-        return;
-      }
-      const stmt = db.prepare("INSERT INTO books VALUES (?, ?)");
-      stmt.run([1, "走れメロス"], (err) => {
+    db.run(
+      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL)",
+      (err) => {
         if (err) {
           callback(err);
           return;
         }
-        stmt.run([2, "こころ"], (err) => {
+        const stmt = db.prepare("INSERT INTO books (title) VALUES (?)");
+        stmt.run(["走れメロス"], function (err) {
           if (err) {
             callback(err);
             return;
           }
-          stmt.run([3, "山月記"], (err) => {
-            stmt.finalize();
+          stmt.run(["こころ"], function (err) {
             if (err) {
               callback(err);
               return;
             }
-            db.all("SELECT id, title FROM books ORDER BY id", (err, rows) => {
+            stmt.run(["山月記"], function (err) {
+              stmt.finalize();
               if (err) {
                 callback(err);
                 return;
               }
-              callback(null, rows);
+              db.all("SELECT id, title FROM books ORDER BY id", (err, rows) => {
+                if (err) {
+                  callback(err);
+                  return;
+                }
+                callback(null, rows);
+              });
             });
           });
         });
-      });
-    });
+      },
+    );
   }, 1000);
 }
 

1fc02542e50b6f1e3f0d7a6faab91f17def74d79
koguchi-e <koguchi.e915@gmail.com>
2026-01-09 13:47:06 +0900

    コールバックを使って非同期処理の実行順が正しくなるよう修正

--- 03.asynchronous/call_back.js
+++ 03.asynchronous/call_back.js
@@ -5,65 +5,60 @@ const db = new sqlite3.Database(":memory:");
 
 // エラーなし
 function runBooksFlow(callback) {
-  setTimeout(() => {
-    db.run(
-      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)",
-      () => {
-        console.log("レコードを追加し、自動採番された ID を標準出力に出力する");
-        const stmt = db.prepare("INSERT INTO books (title) VALUES (?)");
-        stmt.run(["走れメロス"], function () {
-          console.log(this.lastID + "：走れメロス");
-          stmt.run(["こころ"], function () {
-            console.log(this.lastID + "：こころ");
-            stmt.run(["山月記"], function () {
-              console.log(this.lastID + "：山月記");
-              stmt.finalize();
-              db.all("SELECT id, title FROM books ORDER BY id", (err, rows) => {
-                db.run("DROP TABLE IF EXISTS books", () => {
-                  callback(null, rows);
-                });
+  db.run(
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)",
+    () => {
+      console.log("レコードを追加し、自動採番された ID を標準出力に出力する");
+      const stmt = db.prepare("INSERT INTO books (title) VALUES (?)");
+      stmt.run(["走れメロス"], function () {
+        console.log(this.lastID + "：走れメロス");
+        stmt.run(["こころ"], function () {
+          console.log(this.lastID + "：こころ");
+          stmt.run(["山月記"], function () {
+            console.log(this.lastID + "：山月記");
+            stmt.finalize();
+            db.all("SELECT id, title FROM books ORDER BY id", (err, rows) => {
+              db.run("DROP TABLE IF EXISTS books", () => {
+                callback(null, rows);
               });
             });
           });
         });
-      },
-    );
-  }, 1000);
+      });
+    },
+  );
+}
+
+// エラーあり
+function errorFlow(callback) {
+  db.run(
+    "CREATE TABLE books_error (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)",
+    (err) => {
+      if (err) return callback(err);
+      db.run("INSERT INTO books_error (title) VALUES (null)", (err) => {
+        if (err) {
+          db.run("DROP TABLE IF EXISTS books_error", () => {
+            callback(err);
+          });
+        }
+        db.all("SELECT id, author FROM books_error", (err) => {
+          db.run("DROP TABLE IF EXISTS books_error", () => {
+            callback(err);
+          });
+        });
+      });
+    },
+  );
 }
 
 runBooksFlow((err, rows) => {
   console.log("レコードを取得し、それを標準出力に出力する");
+
   rows.forEach((row) => {
     console.log(row.id + ": " + row.title);
   });
-});
 
-// エラーあり
-function errorFlow(callback) {
-  setTimeout(() => {
-    db.run(
-      "CREATE TABLE books_error (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)",
-      (err) => {
-        if (err) return callback(err);
-        db.run("INSERT INTO books_error (title) VALUES (null)", (err) => {
-          if (err) {
-            db.run("DROP TABLE IF EXISTS books_error", () => {
-              callback(err);
-            });
-          }
-          db.all("SELECT id, author FROM books_error", (err) => {
-            db.run("DROP TABLE IF EXISTS books_error", () => {
-              callback(err);
-            });
-          });
-        });
-      },
-    );
-  }, 2000);
-}
-
-errorFlow((err) => {
-  if (err) {
-    console.error("エラー：", err.message);
-  }
+  errorFlow((err) => {
+    if (err) console.error("エラー：", err.message);
+  });
 });

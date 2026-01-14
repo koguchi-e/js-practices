2a0e01dc911b46853ccc53b98028227e7b089a4c
koguchi-e <koguchi.e915@gmail.com>
2026-01-14 15:33:44 +0900

    Top-level await を使い、関数定義を削除

--- 03.asynchronous/async_await.js
+++ 03.asynchronous/async_await.js
@@ -2,49 +2,45 @@
 
 import { runAsync, allAsync } from "./db.js";
 
-async function asyncAwait() {
-  try {
-    // エラーなし
-    console.log("レコードを追加し、自動採番された ID を標準出力に出力する");
-    await runAsync(
-      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)",
-    );
-    const firstInsertResult = await runAsync(
-      "INSERT INTO books (title) VALUES ('走れメロス')",
-    );
-    console.log(`${firstInsertResult.lastID}：走れメロス`);
-    const secondInsertResult = await runAsync(
-      "INSERT INTO books (title) VALUES ('こころ')",
-    );
-    console.log(`${secondInsertResult.lastID}：こころ`);
-    const thirdInsertResult = await runAsync(
-      "INSERT INTO books (title) VALUES ('山月記')",
-    );
-    console.log(`${thirdInsertResult.lastID}：山月記`);
-    console.log("レコードを取得し、それを標準出力に出力する");
-    const rows = await allAsync("SELECT id, title FROM books ORDER BY id");
-    rows.forEach((row) => {
-      console.log(row.id + ": " + row.title);
-    });
-    await runAsync("DROP TABLE books");
-  } catch (err) {
-    console.error("エラー：", err.message);
-  }
-  // エラーあり
+try {
+  // エラーなし
+  console.log("レコードを追加し、自動採番された ID を標準出力に出力する");
   await runAsync(
-    "CREATE TABLE books_error (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)",
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)",
   );
-  try {
-    await runAsync("INSERT INTO books_error (title) VALUES (null)");
-  } catch (insertErr) {
-    console.error("INSERTエラー：", insertErr.message);
-  }
-  try {
-    await allAsync("SELECT id, author FROM books");
-  } catch (selectErr) {
-    console.error("SELECTエラー：", selectErr.message);
-  }
-  await runAsync("DROP TABLE books_error");
+  const firstInsertResult = await runAsync(
+    "INSERT INTO books (title) VALUES ('走れメロス')",
+  );
+  console.log(`${firstInsertResult.lastID}：走れメロス`);
+  const secondInsertResult = await runAsync(
+    "INSERT INTO books (title) VALUES ('こころ')",
+  );
+  console.log(`${secondInsertResult.lastID}：こころ`);
+  const thirdInsertResult = await runAsync(
+    "INSERT INTO books (title) VALUES ('山月記')",
+  );
+  console.log(`${thirdInsertResult.lastID}：山月記`);
+  console.log("レコードを取得し、それを標準出力に出力する");
+  const rows = await allAsync("SELECT id, title FROM books ORDER BY id");
+  rows.forEach((row) => {
+    console.log(row.id + ": " + row.title);
+  });
+  await runAsync("DROP TABLE books");
+} catch (err) {
+  console.error("エラー：", err.message);
 }
-
-asyncAwait();
+// エラーあり
+await runAsync(
+  "CREATE TABLE books_error (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)",
+);
+try {
+  await runAsync("INSERT INTO books_error (title) VALUES (null)");
+} catch (insertErr) {
+  console.error("INSERTエラー：", insertErr.message);
+}
+try {
+  await allAsync("SELECT id, author FROM books");
+} catch (selectErr) {
+  console.error("SELECTエラー：", selectErr.message);
+}
+await runAsync("DROP TABLE books_error");

fc557601fc99c8df4dbd6a2d84dae9d27a7507bf
koguchi-e <koguchi.e915@gmail.com>
2026-01-14 14:31:07 +0900

    コールバックと同じ処理構造になるよう、プロミス版を修正

--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -6,23 +6,23 @@ import { runAsync, allAsync } from "./db.js";
 runAsync(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)",
 )
-  .then(() =>
-    console.log("レコードを追加し、自動採番された ID を標準出力に出力する"),
-  )
-  .then(() => runAsync("INSERT INTO books (title) VALUES ('走れメロス')"))
+  .then(() => {
+    console.log("レコードを追加し、自動採番された ID を標準出力に出力する");
+    return runAsync("INSERT INTO books (title) VALUES ('走れメロス')");
+  })
   .then((stmt) => {
     console.log(`${stmt.lastID}：走れメロス`);
+    return runAsync("INSERT INTO books (title) VALUES ('こころ')");
   })
-  .then(() => runAsync("INSERT INTO books (title) VALUES ('こころ')"))
   .then((stmt) => {
     console.log(`${stmt.lastID}：こころ`);
+    return runAsync("INSERT INTO books (title) VALUES ('山月記')");
   })
-  .then(() => runAsync("INSERT INTO books (title) VALUES ('山月記')"))
   .then((stmt) => {
     console.log(`${stmt.lastID}：山月記`);
+    console.log("レコードを取得し、それを標準出力に出力する");
+    return allAsync("SELECT id, title FROM books ORDER BY id");
   })
-  .then(() => console.log("レコードを取得し、それを標準出力に出力する"))
-  .then(() => allAsync("SELECT id, title FROM books ORDER BY id"))
   .then((rows) => {
     rows.forEach((row) => {
       console.log(row.id + ": " + row.title);

60bbc3c74509e991c49d50eb67aed31cdfd37f30
koguchi-e <koguchi.e915@gmail.com>
2026-01-15 11:48:59 +0900

    sqlite3 を Promise 化したファイルの名称をより適切なものに変更

--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import sqlite3 from "sqlite3";
-import { runAsync, allAsync } from "./db.js";
+import { runAsync, allAsync } from "./sqlite3-promise.js";
 
 const db = new sqlite3.Database(":memory:");
 
